ee3fe06f5b9d147ae17bc06386309863a4565b06
Aquariuslt <SuperAquariuslt@Gmail.com>
2016-03-20 15:20:46 +0800

    update class for divide 'templates' and 'webpack'

--- src/client/app/common/controllers/header-controller.js
+++ src/client/app/common/controllers/header-controller.js
@@ -2,24 +2,19 @@
 'use strict';
 
 
+module.exports = function headerController($scope, $timeout, $mdSidenav, $log) {
 
-module.exports = function headerController($scope, $timeout, $mdSidenav, $log){
-
-
-
-
+  
   $scope.toggleLeft = buildDelayedToggler('left');
 
 
-
-
-  function debounce(func, wait, context) {
+  function debounce(func, wait) {
     var timer;
     return function debounced() {
       var context = $scope,
         args = Array.prototype.slice.call(arguments);
       $timeout.cancel(timer);
-      timer = $timeout(function() {
+      timer = $timeout(function () {
         timer = undefined;
         func.apply(context, args);
       }, wait || 10);
@@ -27,7 +22,7 @@ module.exports = function headerController($scope, $timeout, $mdSidenav, $log){
   }
 
   function buildDelayedToggler(navID) {
-    return debounce(function() {
+    return debounce(function () {
       $mdSidenav(navID)
         .toggle()
         .then(function () {
