6a46f90a39d856d64aefd3aba5869c6791d5d7da
Aquariuslt <SuperAquariuslt@Gmail.com>
2017-03-01 01:22:55 +0800

    header animation

--- src/app/core/nav-header/nav-header.component.ts
+++ src/app/core/nav-header/nav-header.component.ts
@@ -1,27 +1,50 @@
-import {Component} from "@angular/core";
+import {Component, trigger, transition, style, animate} from "@angular/core";
+import {LogFactory} from "../../shared/log.factory";
 
 @Component({
   selector: 'nav-header',
   templateUrl: './nav-header.component.html',
-  styleUrls: ['./nav-header.component.css']
+  styleUrls: ['./nav-header.component.css'],
+  animations: [
+    trigger('sidenavState', [
+      transition('show => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('0.2s ease-in')
+      ]),
+      transition('* => show', [
+        style({
+          opacity: 0,
+          transform: 'translateX(100%)'
+        }),
+        animate('0.2s ease-in')
+      ])
+    ])
+  ]
 })
 export class NavHeaderComponent {
 
-  constructor() {
+  constructor(private logFactory:LogFactory) {
   }
+  
+  private logger = this.logFactory.getLog(NavHeaderComponent.name);
+  
+  searchInputOpened: boolean = false;
+  sideNavbarOpened: boolean = false;
+  sideNavbarShowStatus: string = 'show';
 
-  searchInputOpened:boolean = false;
-  sideNavbarOpened:boolean = false;
-
-
-  toggleSearchInput():void{
+  toggleSearchInput(): void {
     let vm = this;
     vm.searchInputOpened = !vm.searchInputOpened;
-  }
+    }
 
-  toggleNavSidebar():void{
+  toggleNavSidebar(): void {
     let vm = this;
     vm.sideNavbarOpened = !vm.sideNavbarOpened;
+    vm.sideNavbarShowStatus = vm.sideNavbarShowStatus == 'show' ? 'hide' : 'show';
+    vm.logger.info('navbar show status:',vm.sideNavbarShowStatus);
   }
 
 }
