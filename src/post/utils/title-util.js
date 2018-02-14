6f5bda4bdcd001f580cbc6f0ccaab152501cd256
Aquariuslt <superaquariuslt@gmail.com>
2018-02-14 20:54:09 +0800

    [UT] Add UnitTest for title-util.

--- src/post/utils/title-util.js
+++ src/post/utils/title-util.js
@@ -1,16 +1,24 @@
-import _ from 'lodash';
-
 const SPACER = ' | ';
 const COLON = ' : ';
 
+/**
+ * @param {String} title
+ * */
 function setTitle(title) {
   document.title = title + SPACER + document.title;
 }
 
+/**
+ * @param {Object} post
+ * */
 function setPostTitle(post) {
   document.title = post.metadata.title + SPACER + document.title;
 }
 
+/**
+ * @param {String} filterType
+ * @param {String} filterValue
+ * */
 function setFilteredTitle(filterType, filterValue) {
   document.title = filterType + COLON + filterValue + SPACER + document.title;
 }
