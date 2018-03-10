1532cfd69cc1de5696a71aac751e6a7af14f22d0
Aquariuslt <superaquariuslt@gmail.com>
2018-03-10 21:23:18 +0800

    [UX] Update transaction animation.

--- src/App.vue
+++ src/App.vue
@@ -32,16 +32,10 @@
 </script>
 
 <style scoped>
-  .fade-enter-active {
-    transition: all .3s ease;
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
   }
-
-  .fade-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  }
-
   .fade-enter, .fade-leave-to {
-    transform: translateX(10px);
     opacity: 0;
   }
 </style>
