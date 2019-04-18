60bce2b45b4a90e0ddd3108e29562d890d4ca536
aquariuslt <superaquariuslt@gmail.com>
2019-04-19 03:21:41 +0800

    ci: add linting report

--- tasks/gulp/lint.ts
+++ tasks/gulp/lint.ts
@@ -1,11 +1,15 @@
 import * as gulp from 'gulp';
 import * as eslint from 'gulp-eslint';
+import * as fs from 'fs';
 
 const TS_FILE_PATTERN = '**/*.ts';
 
 gulp.task('lint', (): void => {
   return gulp.src(TS_FILE_PATTERN)
     .pipe(eslint())
+    .pipe(eslint.format('junit', (result: string): void => {
+      fs.writeFileSync('reports/junit/js-lint-results.xml', result);
+    }))
     .pipe(eslint.failAfterError())
     ;
 });
