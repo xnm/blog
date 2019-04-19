55b23157ae82d982f1a60a192d6c3ef72935030d
aquariuslt <superaquariuslt@gmail.com>
2019-04-19 23:35:56 +0800

    chore: remove compile task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,6 @@
 import './tasks/gulp/clean';
 import './tasks/gulp/build';
 import './tasks/gulp/serve';
-import './tasks/gulp/compile';
 import './tasks/gulp/test';
 import './tasks/gulp/lint';
 import './tasks/gulp/ci';
