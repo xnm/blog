a8839e012db081f0c155e3203ee8fd5e7bf7fbd8
aaroncui <aaroncui@tencent.com>
2019-08-11 04:25:31 +0800

    fix: google-site-verification code

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,15 +1,17 @@
 import * as config from '@/config.json';
 
 if (config.features.ga) {
-  const googleAnalyticsMeta = document.getElementById('google-analytics');
-  if (googleAnalyticsMeta) {
-    googleAnalyticsMeta.setAttribute('content', config.features.ga.verification);
+  const googleSiteVerificationTag = document.getElementById('google-site-verification');
+  if (googleSiteVerificationTag) {
+    googleSiteVerificationTag.setAttribute('content', config.features.ga.verification);
 
     // @ts-ignore
-    window.ga = window.ga || function() {
-      (ga.q = ga.q || []).push(arguments);
-    };
-    ga.l = +new Date;
+    window.ga =
+      window.ga ||
+      function() {
+        (ga.q = ga.q || []).push(arguments);
+      };
+    ga.l = +new Date();
     ga('create', config.features.ga.tracking, 'auto');
     ga('send', 'pageview');
   }
