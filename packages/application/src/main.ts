73022f82107631c0ab9deed9b13d43483a2b5781
aquariuslt <superaquariuslt@gmail.com>
2019-10-04 12:32:45 +0800

    chore: initial sub package application

---
+++ packages/application/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@/app.module';
+
+async function bootstrap() {
+  const port = 5000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(port);
+}
+
+bootstrap().then();
