42815ccedd0022477ad62b61702e69dae371e7f3
aquariuslt <superaquariuslt@gmail.com>
2019-11-30 17:20:40 +0800

    perf: disable offline google-analytics

--- packages/pwa-tools/src/service-worker.ts
+++ packages/pwa-tools/src/service-worker.ts
@@ -6,7 +6,6 @@ export const buildServiceWorker = async (dest: string) => {
     globPatterns: ['**/*.{js,json,css}', '**/*.{jpg,jpeg,png}', '**/*.{ttf,woff,woff2,eot}', '**/*.html', 'index.html'],
     importWorkboxFrom: 'local',
     swDest: `${dest}/service-worker.js`,
-    offlineGoogleAnalytics: true,
     skipWaiting: true,
     clientsClaim: true
   });
