b5ea736d5eca664b82131f177b9d1500a3cf61d1
aquariuslt <superaquariuslt@gmail.com>
2020-02-13 23:13:35 +0800

    style(theme-react): update fonts

--- themes/theme-react/src/constants.ts
+++ themes/theme-react/src/constants.ts
@@ -3,6 +3,14 @@
  **/
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+export const THEME_FONT_FAMILY =
+  'Roboto, XHei, -apple-system, BlinkMacSystemFont, PingFang SC, Hiragino Sans GB, Microsoft YaHei,\n' +
+  '    WenQuanYi Micro Hei, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol';
+export const DRAWER_WIDTH = 240;
+export const CARD_MAX_WIDTH = 800;
+
+export const TYPE_JSON_LD = 'application/ld+json';
+
 export const theme = createMuiTheme({
   palette: {
     primary: {
@@ -15,10 +23,15 @@ export const theme = createMuiTheme({
       paper: '#F8F9FA',
       default: '#F5F5F5'
     }
+  },
+  overrides: {
+    MuiTypography: {
+      root: {
+        fontFamily: THEME_FONT_FAMILY
+      },
+      body2: {
+        fontFamily: THEME_FONT_FAMILY
+      }
+    }
   }
 });
-
-export const DRAWER_WIDTH = 240;
-export const CARD_MAX_WIDTH = 800;
-
-export const TYPE_JSON_LD = 'application/ld+json';
