324032f5b903855f96fbd150b771871ad398cbf6
aquariuslt <superaquariuslt@gmail.com>
2020-03-14 18:38:04 +0800

    fix(pwa): resolve globPattern issue

--- packages/pwa/src/service-worker.ts
+++ packages/pwa/src/service-worker.ts
@@ -3,7 +3,7 @@ import * as workbox from 'workbox-build';
 export const buildServiceWorker = async (dest: string) => {
   await workbox.generateSW({
     globDirectory: dest,
-    globPatterns: ['**/*.{js,json,css}', '**/*.{webp}', '**/*.{ttf,woff,woff2,eot}', '**/*.html', 'index.html'],
+    globPatterns: ['**/*.{js,json,css,html,ttf,woff,woff2,eot,webp}'],
     importWorkboxFrom: 'local',
     swDest: `${dest}/service-worker.js`,
     skipWaiting: true,
